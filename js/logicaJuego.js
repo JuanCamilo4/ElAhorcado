6739e29903d4dc66acaf9c92f03f501dd2b10a97
Juan K <juanr_0404@hotmail.com>
2021-07-11 15:14:10 -0500

    Adición de palabras

--- js/logicaJuego.js
+++ js/logicaJuego.js
@@ -15,11 +15,168 @@ $(document).ready(function () {
 
     //Inicializar variables y constantes
     let palabras = {
-        perro : ["Tiene 4 patas", "Tiene cola", "Le gusta jugar con los humanos"],
-        gato : ["Tiene 4 patas", "Tiene cola"],
-        hamster: ["Le gusta las semillas de girasol", "Tiene cola larga", "Tiene 4 pata"],
-        pajaro: ["Tiene 2 patas", "Tiene plumas", "Puede volar"],
-        tigre: ["Es carnivoro", "Se camufla muy bien", "Caza de noche", "Tiene dientes largos"]
+        perro : [
+            "Es un animal mamífero", 
+            "Se puede considerar un animal domestico", 
+            "Tiene 4 patas",
+            "Tiene cola", 
+            "Es muy cariñoso con los humanos", 
+            "Tiene un sentido del olfato muy agudo", 
+            "Tiene un  hocico alargado"
+        ],
+        gato : [
+            "Es un animal mamífero",
+            "Se puede considerar un animal domestico",
+            "Tiene 4 patas",
+            "Tiene cola",
+            "Puede mostrar y ocultar sus garras a voluntad",
+            "Cuentan con una gran flexibilidad y elasticidad",
+            "Es una animal carnívoro",
+            "Son de hábitos solitarios."
+        ],
+        hamster: [
+            "Es un animal mamífero",
+            "Le gusta las semillas de girasol",
+            "Pesa entre 30 a 180 gr y puede medir hasta 18cm",
+            "Tiene 4 pata",
+            "Es de reproducción vivípara",
+            "Es omnivoro",
+            "Su esperanza de vida es de 2 a 3 años",
+            "Acotumbra estar en ambientes nocturnos"
+        ],
+        pájaro: [
+            "Es un animal",
+            "Tiene 2 patas",
+            "Tiene plumas",
+            "Puede volar",
+            "Tiene pico",
+            "Son de sangre caliente"
+        ],
+        tigre: [
+            "Es un animal terrestre",
+            "Es carnívoro",
+            "Vive exclusivamente en Asia",
+            "Viven de forma solitaria",
+            "Los machos pueden llegar a medir hasta 330cm y las hembras hasta 275cm",
+            "Están en peligro de extinción",
+            "Cuenta con un pelaje rayado",
+            "Los machos suelen ser bastantes territoriales"
+        ],
+        serpiente: [
+            "Es un animal terrestre",
+            "Son reptiles",
+            "Algunas especies presentan un veneno tan potente que puede llegar a ser letal",
+            "No poseen patas", "Son carnívoras",
+            "Son muy sensibles a las vibraciones del suelo",
+            "La mayoria viven en zonas cálidas o de clima moderado",
+            "Para capturar a sus presas los paralizan y después los mata",
+            "Su piel se compone de escamas epidérmicas"
+        ],
+        pez: [
+            "Es un animal marino",
+            "Son vertebrados",
+            "Su cuerpo está recubierto por escamas",
+            "Tienen temperatura variable",
+            "Se reproducen por huevos",
+            "Su circulación es sencilla y completa",
+            "Son de gran importancia tanto econónomica como alimenticia para el ser humano",
+            "Al ingerirlo se pueden diferentes vitaminas",
+            "Con el se pueden realizar diferentes productos industriales",
+            "Son de sangre fría"
+        ],
+        mono: [
+            "Es un animal terrestre",
+            "Son primates con una gran inteligencia",
+            "Se encuentran mayormente en centroamerica y sudamerica",
+            "Se asemeja físicamente a un humano",
+            "La gran mayoría vive en los arboles",
+            "Les gusta los platanos",
+            "Se pueden encontrar en zonas cálidas y selváticas"
+        ],
+        hipopótamo: [
+            "Es un animal terrestre",
+            "Es un mamífero",
+            "Posee un cuerpo muy grande, mucho lo comparan con un barril",
+            "Tiene grandes dientes",
+            "Por lo general tienen patas cortas y gruesas",
+            "Generalmente pesan 3 toneladas",
+            "Suele ser bastante agresivo",
+            "Suelen comer 150 libras de pasto",
+            "Tienen una piel rojiza",
+            "Habitan principalmente en los lagos y ríos de África",
+            "Se alimentan de platna y hierbas"
+        ],
+        zorro: [
+            "Es un animal terrestre",
+            "Tiene 4 patas",
+            "Es mamífero",
+            "Su apariencia es semejante a la de un perro de tamaño medio",
+            "Se alimentan de lo que sea",
+            "Tiene hábitos de cazador y depredador",
+            "Emplean su fino olfato y oído para detectar pequeños roedores",
+            "Aunque viven en todos los contienentes se sabe que las especies más comunes prefieren cilmas temperados",
+            "Viven entre 5 y 7 años",
+            "Es la especie animal más ampliamente repartida en el mundo"
+        ],
+        jirafa: [
+            "Es un animal terrestre",
+            "Es el animal más alto del mundo",
+            "Es un mamífero",
+            "Habita en Africa, principalmentes en sabanas, praderas, bosques y campos",
+            "Es un animal terrestre",
+            "Los machos pesan alrededor de 1600kg y las hembras alcazan los 825kg",
+            "Son herbívoras y es comun verlas comiendo frutas frescas de las copas de los árboles",
+            "Estos animales beben poca agua y pueden pasar 3 días sin hacerlo",
+            "Este animal se encuentra en peligro de extinción"
+        ],
+        pulpo: [
+            "Es un animal marino invertebrado",
+            "Estan dentro del orden de los octópodos",
+            "Es considerado el molusco más inteligente",
+            "Se encuentran practicamente en todos los océanos, en especial en los arrecifes coralinos y otras regiones que les faciliten el escondite",
+            "Son básicamentes omnivoros, osea que pueden comer cualquier cosa",
+            "Son capaces de imitar hasta 15 diferentes especies",
+            "Cuenta con depósitos de tinta negra que puede emitir en el agua",
+            "Cuentan con mordedura venenosa",
+            "Su piel es un perfecto mecanismo de camuflaje",
+            "Este animal es considerado un manjar en muchas gastronomías costeras"
+        ],
+        ballena: [
+            "Es un animal marino",
+            "Es de los mamíferos con mayor tamaño",
+            "Son animales de sangre caliente",
+            "Miden entre 15 y 17 metros",
+            "Pesan entre 50 y 80 toneladas",
+            "Ellas respiran tomando el aire a través de su espiráculo",
+            "Se alimenta de peces, crustáceos, calamares, camarones, krill",
+            "Viven alrededor de 30 años",
+            "Tienen pelo en su superficie, pero es muy fino",
+            "Viven en grupos pequeños",
+            "Pueden sumergirse a una gran profundidad"
+        ],
+        rinoceronte: [
+            "Es un animal terrestre",
+            "Estos animales suelen pesar entre 800 y 1400 kg",
+            "Son solitarios y muy territoriales",
+            "Las esperanza de vida en las condiciones más favorables es de 60 años",
+            "Se pueden encontrar viviendo en África, Asia, India, Malasia e Indonesia",
+            "Este animal se encuentra en peligro de extinción",
+            "Cuenta con un gran cuerno en la cabeza, este mismo es bastante codiciado por el humano",
+            "Son anmales herbívoros"
+        ],
+        elefante: [
+            "Es de los animales terrestre más grandes",
+            "Es mamífero",
+            "Su inteligencia en algunos casos es similar a la de los seres humanos",
+            "Suelen pesar 7000kg",
+            "Viven entre 60 y 70 años",
+            "Este animal se encuentra en peligro de extinción",
+            "Cuenta con una larga trompa con diferentes funciones como emitir sonidos y distinguir sonidos",
+            "Cuentan con dos colmillos en forma curva",
+            "Cuentan con grandes orejas, las cuales están muy vascularizadas",
+            "Son animales herbívoros, ósea comen plantas",
+            "Viven tanto enselvas como en sabanas"
+        ]
     }
     let tiempo = {
         milisegundo : 0,
@@ -27,7 +184,7 @@ $(document).ready(function () {
         minuto: 0,
         horas: 0
     };
-    let datosJugador = {
+    let datosJugador = { //Datos que se van a mostrar en la tabla del historial
         nombre: "",
         tiempo: 0,
         resultado: ""
@@ -52,6 +209,7 @@ $(document).ready(function () {
             activdorCronometro();
             inicializarPalabra();
             respuestaPlayer.focus();
+            console.warn("No intentes hacer nada malo");
         }
     });
     containerPantallaInicio.keydown(function (e) { 
@@ -61,6 +219,7 @@ $(document).ready(function () {
                 activdorCronometro();
                 inicializarPalabra();
                 respuestaPlayer.focus();
+                console.warn("No intentes hacer nada malo");
             }
         }
     });
@@ -114,6 +273,7 @@ $(document).ready(function () {
             }
             posicionPalabraSeleccionada++;
         }
+        palabraSeleccionada = darFormato(palabraSeleccionada);
         return palabraSeleccionada;
         
     }
@@ -127,14 +287,16 @@ $(document).ready(function () {
             aux += "_ ";
         }
         palabraSistema.text(aux);
-        console.log(palabra)
+        //console.log(palabra)
     }
     inicializarPalabra();
-    console.log(palabra)
+    //console.log(palabra)
     const buscarLetra = () =>{
         respuestaPlayerAux = respuestaPlayer.val().trim().toLowerCase() //Obtiene valor, LE quita los espacios y Lo comvierte en minuscula
+        respuestaPlayerAux = darFormato(respuestaPlayerAux);
+        //console.log(respuestaPlayerAux);
 
-        if(palabra.includes(respuestaPlayerAux) && respuestaPlayerAux != '' && respuestaPlayerAux != null) return true; //Verifica si la palabra se encuentra
+        if(palabra.includes(respuestaPlayerAux)) return true; //Verifica si la palabra se encuentra
         return false
         
     }
@@ -162,6 +324,7 @@ $(document).ready(function () {
         let respuestaPlayerAux = respuestaPlayer.val() //Obtiene valor
         let respuestaPlayerSinEspacios = respuestaPlayerAux.trim(); //LE quita los espacions
         let respuestaPlayerFinal = respuestaPlayerSinEspacios.toLowerCase(); //Lo comvierte en minuscula
+        respuestaPlayerFinal = darFormato(respuestaPlayerFinal);
 
         if (resultadoPlayer) {
             for(let i = 0; i < palabra.length; i++){
@@ -170,7 +333,7 @@ $(document).ready(function () {
                 }
             }
         }
-        console.log(arrayPalabra);
+        //console.log(arrayPalabra);
         palabraSistema.text(arrayPalabra.join(' '));
     }
     let pistaSeleccionada = 0;
@@ -187,10 +350,11 @@ $(document).ready(function () {
     }
     const jugar = () =>{
         respuestaPlayerAux = respuestaPlayer.val().trim().toLowerCase()
+        respuestaPlayerAux = darFormato(respuestaPlayerAux);
         if (buscarLetra()) {
-            console.log(buscarLetra());
+            //console.log(buscarLetra());
         } else {
-            console.log(buscarLetra());
+            //console.log(buscarLetra());
         }
         managerMuñeco(buscarLetra());
         validarPalabra(buscarLetra());
@@ -242,10 +406,10 @@ $(document).ready(function () {
         } else {
             datosJugador['resultado'] = "Perdió";
         }
-        console.log(datosJugador);
+        //console.log(datosJugador);
         numeroJugador = localStorage.getItem('numeroJugador');
         numeroJugador++;
-        console.log(numeroJugador);
+        //console.log(numeroJugador);
         localStorage.setItem(`jugador${numeroJugador}`, JSON.stringify(datosJugador));
         localStorage.setItem('numeroJugador', numeroJugador);
     }
@@ -258,10 +422,27 @@ $(document).ready(function () {
                 "<tr class='resultado-partida'>"+
                     `<th>${JSON.parse(localStorage.getItem(`jugador${i}`))['nombre']}</th>`+
                     `<th>${JSON.parse(localStorage.getItem(`jugador${i}`))['tiempo']}</th>`+
-                    `<th style="color: #FFD369;">${JSON.parse(localStorage.getItem(`jugador${i}`))['resultado']}</th>`+
+                    `<th>${JSON.parse(localStorage.getItem(`jugador${i}`))['resultado']}</th>`+
                 "</tr>"
             );
         }
+    }
+    function darFormato(cadena){
+        let aMayus = cadena.replace(/[ÁÀÄÂ]/g,'A');
+        let aMinus = aMayus.replace(/[áàäâ]/g,'a');
+        let eMayus = aMinus.replace(/[ÉÈËÊ]/g,'E');
+        let eMinus = eMayus.replace(/[éèëê]/g,'e');
+        let iMayus = eMinus.replace(/[ÍÌÏÎ]/g,'I');
+        let iMinus = iMayus.replace(/[íìïî]/g,'i');
+        let oMayus = iMinus.replace(/[ÓÒÖÔ]/g,'O');
+        let oMinus = oMayus.replace(/[óòöô]/g,'o');
+        let uMayus = oMinus.replace(/[ÚÙÜÛ]/g,'U');
+        let uMinus = uMayus.replace(/[úùüû]/g,'u');
+        let enieMayus = uMinus.replace(/[Ñ]/g,'N');
+        let enieMinus = enieMayus.replace(/[ñ]/g,'n');
+        let resultado = enieMinus.replace(/['|°¬!^`~"#$%&/()Çç=?¿{}_,.´+<>¡¨*:;]/gi,'');
+
+        return resultado;
     }
     const activdorCronometro = () =>{
         intervalContador = setInterval(function(e){contador()}, 10);
